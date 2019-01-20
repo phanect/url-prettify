ec7960f562c90a617c120962ba0089fa774ab463
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-01-20 20:32:13 +0900

    chore: initial commit

---
+++ prettify.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Cache Loading
+if (window.Worker) {
+  const process = () => {
+    const worker = new Worker(browser.extension.getURL("worker.js"));
+
+    worker.postMessage({
+      url: document.URL,
+    });
+
+    worker.onmessage = (e) => {
+      console.log("push")
+      history.pushState({}, "", e.data);
+    };
+  };
+
+  if (document.readyState === "complete") {
+    process();
+  } else {
+    window.addEventListener("load", process, {
+      once: true,
+      passive: true,
+    });
+  };
+}
---
+++ worker.js
@@ -0,0 +1,50 @@
+"use strict";
+
+onmessage = function(evt) {
+  const url = new URL(evt.data.url),
+        params = new URLSearchParams(url.search);
+
+  // Google Analytics
+  params.delete("utm_source");
+  params.delete("utm_medium");
+  params.delete("utm_campaign");
+  // Google Ads
+  params.delete("gclid");
+  // Facebook
+  params.delete("fbclid");
+
+  if (url.hostname === "www.youtube.com") {
+    params.delete("feature");
+  }
+
+  if (
+    url.hostname === "www.amazon.com" ||
+    url.hostname === "www.amazon.co.jp"
+  ) {
+    params.delete("tag");
+    params.delete("creative");
+    params.delete("creativeASIN");
+    params.delete("linkCode");
+    params.delete("hvadid");
+    params.delete("hvpos");
+    params.delete("hvnetw");
+    params.delete("hvrand");
+    params.delete("hvpone");
+    params.delete("hvptwo");
+    params.delete("hvqmt");
+    params.delete("hvdev");
+    params.delete("hvdvcmdl");
+    params.delete("hvlocint");
+    params.delete("hvlocphy");
+    params.delete("hvtargid");
+  }
+
+  if (url.hostname === "item.rakuten.co.jp") {
+    params.delete("scid");
+    params.delete("sc2id");
+  }
+
+  const queryString = params.toString();
+
+  postMessage(url.origin + url.pathname + (queryString.length > 0 ? "?" + queryString : ""));
+}
